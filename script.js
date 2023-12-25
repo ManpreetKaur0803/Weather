fcd2855de21ac9f2e91e02f1c5b076cb7e58b11a
ManpreetKaur0803 <mini08032003@gmail.com>
2023-12-25 22:41:34 +0530

    weather casting

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ async function checkWeather(city){
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data = await response.json();
 
-    console.log(data);
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
@@ -32,6 +31,8 @@ async function checkWeather(city){
     else if(data.weather[0].main == "Drizzle"){
         cloudy.src = "images/drizzle.png";
     }
+
+    document.querySelector(".weather").style.display = "block"; 
 }
 searchBtn.addEventListener("click",()=>{
     checkWeather(searchBox.value);
