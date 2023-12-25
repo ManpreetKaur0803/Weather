54b77f3f8a3e34e482cfd1b6c64ab579009a86fb
ManpreetKaur0803 <mini08032003@gmail.com>
2023-12-25 23:45:28 +0530

    Weather Casting

--- script.js
+++ script.js
@@ -7,32 +7,42 @@ const cloudy = document.querySelector(".cloudy");
 
 async function checkWeather(city){
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-    var data = await response.json();
+    
+    if(response.status == 404){
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    }
+    else{
+        var data = await response.json();
 
-    document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
-    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-    document.querySelector(".wind").innerHTML = data.wind.speed +" km/h";
-    if(data.weather[0].main == "Clouds"){
-        cloudy.src = "images/clouds.png";
-    }
-    else if(data.weather[0].main == "Rain"){
-        cloudy.src = "images/rain.png";
-    }
-    else if(data.weather[0].main == "Mist"){
-        cloudy.src = "images/mist.png";
-    }
-    else if(data.weather[0].main == "Clear"){
-        cloudy.src = "images/clear.png";
-    }
-    else if(data.weather[0].main == "Snow"){
-        cloudy.src = "images/snow.png";
-    }
-    else if(data.weather[0].main == "Drizzle"){
-        cloudy.src = "images/drizzle.png";
-    }
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed +" km/h";
+    
+        if(data.weather[0].main == "Clouds"){
+            cloudy.src = "images/clouds.png";
+        }
+        else if(data.weather[0].main == "Rain"){
+            cloudy.src = "images/rain.png";
+        }
+        else if(data.weather[0].main == "Mist"){
+            cloudy.src = "images/mist.png";
+        }
+        else if(data.weather[0].main == "Clear"){
+            cloudy.src = "images/clear.png";
+        }
+        else if(data.weather[0].main == "Snow"){
+            cloudy.src = "images/snow.png";
+        }
+        else if(data.weather[0].main == "Drizzle"){
+            cloudy.src = "images/drizzle.png";
+        }
 
-    document.querySelector(".weather").style.display = "block"; 
+        document.querySelector(".weather").style.display = "block"; 
+        document.querySelector(".error").style.display = "none";
+    }
+    
 }
 searchBtn.addEventListener("click",()=>{
     checkWeather(searchBox.value);
